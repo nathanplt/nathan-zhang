452a35a778470985e91518573b7d9459cb55a34e
nathanplt <nathan.l.zhang@gmail.com>
2025-10-22 00:05:32 -0700

    add a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Nathan Zhang",
   description: "Software engineer interested in building high-performance systems, optimizing AI models, and designing distributed data infrastructure.",
+  icons: {
+    icon: "/favicon-cube.svg",
+  },
 };
 
 export default function RootLayout({
